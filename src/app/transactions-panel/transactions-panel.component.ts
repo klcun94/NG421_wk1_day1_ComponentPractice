2f3935c4f7ebe6c0a5cd4b9cdda9edd27ef29c16
klcun94 <klcun94@icloud.com>
2020-03-31 22:37:28 -0500

    transactions panel updated with orders

--- src/app/transactions-panel/transactions-panel.component.ts
+++ src/app/transactions-panel/transactions-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import orders from '../orders';
 
 @Component({
   selector: 'app-transactions-panel',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transactions-panel.component.css']
 })
 export class TransactionsPanelComponent implements OnInit {
-
+  public orders: any[] = [];
   constructor() { }
 
   ngOnInit() {
+    this.orders = orders;
   }
 
 }
